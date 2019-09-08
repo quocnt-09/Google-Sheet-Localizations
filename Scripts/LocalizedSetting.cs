295fcab0db55631c05a687f6445694c1db556ece
quocnt <quoc.nguyen@topebox.com>
2019-09-08 14:15:27 +0700

    first commit source

---
+++ Scripts/LocalizedSetting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LocalizationConfig", menuName = "Google Sheet to Json/Localization Setting", order = 1)]
+public class LocalizedSetting : ScriptableObject
+{
+    public string spreadSheetKey = "";
+    public string startCell = "A1";
+    public string endCell = "Z100";
+    public List<string> SheetNames = new List<string>();
+}
